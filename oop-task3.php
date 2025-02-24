bc59387069865db3cf8cff4633c07d5161e7d262
yasmeen <yasmeen144@yahoo.com>
2025-02-24 10:25:58 +0200

    last

--- oop-task3.php
+++ oop-task3.php
@@ -1,49 +1,64 @@
 <?php
 
-class Session
-{
-    private static function start()
-    {
-        if (session_status() === PHP_SESSION_NONE) session_start();
+class Session {
+    
+    // Start the session if not already started
+    private static function start() {
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
     }
 
-    private static function set($key, $value)
-    {
+    // Set a session key with a value
+    public static function set($key, $value) {
         self::start();
         $_SESSION[$key] = $value;
     }
 
-    private static function get($key)
-    {
+    // Get a session key value if exists
+    public static function get($key) {
         self::start();
-        if (isset($_SESSION[$key])) {
-            return  $_SESSION[$key];
-        }
-        return null;
+        return $_SESSION[$key] ?? null;
     }
 
-    private static function has($key)
-    {
+    // Check if a session key exists
+    public static function has($key) {
         self::start();
-        return (isset($_SESSION[$key]));
+        return isset($_SESSION[$key]);
     }
-    private static function remove($key)
-    {
+
+    // Remove a specific key from the session
+    public static function remove($key) {
         self::start();
         if (isset($_SESSION[$key])) {
             unset($_SESSION[$key]);
         }
     }
 
-    private static function remove_all(){
-      self::start();
-      session_unset();
-      session_destroy();
+    // Remove all session data
+    public static function removeAll() {
+        self::start();
+        session_unset();
+        session_destroy();
     }
 
-    private static function get_all() {
+    // Get all session data
+    public static function getAll() {
         self::start();
-        return $_SESSION ;
+        return $_SESSION;
+    }
+
+    // Flash message: store a message for one request only
+    public static function flash($key, $message = null) {
+        self::start();
+        if ($message !== null) {
+            $_SESSION['flash'][$key] = $message;
+        } elseif (isset($_SESSION['flash'][$key])) {
+            $msg = $_SESSION['flash'][$key];
+            unset($_SESSION['flash'][$key]);
+            return $msg;
+        }
+        return null;
     }
 }
 
@@ -52,30 +67,36 @@ class Session
 
 
 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $name = $_POST['name'] ?? '';
+    
+    // التحقق من صحة الإدخال
+    if (empty($name)) {
+        Session::set('errors', 'الاسم مطلوب!');
+    } else {
+        Session::set('success', 'تم التسجيل بنجاح!');
+        Session::set('name', $name);
+    }
+    
+}
 
-
-
-
-
-
-
+// جلب الإشعارات من الـ Session
+$success = Session::flash('success');
+$error = Session::flash('errors');
 ?>
 
 
-<form action="" method="POST">
-    <input type="text" name="name" placeholder="type your name">
+   
 
+    <form method="POST">
+        <input type="text" name="name" placeholder=" type your name">
+        <button type="submit">send</button>
+    </form>
 
-    <input type="submit" value="send"><br>
-    <input type="submit" value="logout">
-
-
-
-
-
-
-
+    <br>
+    <button type="submit"> logout</a>
+</body>
+</html>
 
 
 
-</form>
