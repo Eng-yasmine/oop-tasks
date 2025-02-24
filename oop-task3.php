db209c7772c4a9e57ada6d314765e8c8ad48d7fa
yasmeen <yasmeen144@yahoo.com>
2025-02-24 10:01:44 +0200

    task3

--- oop-task3.php
+++ oop-task3.php
@@ -1,6 +1,51 @@
 <?php
 
+class Session
+{
+    private static function start()
+    {
+        if (session_status() === PHP_SESSION_NONE) session_start();
+    }
 
+    private static function set($key, $value)
+    {
+        self::start();
+        $_SESSION[$key] = $value;
+    }
+
+    private static function get($key)
+    {
+        self::start();
+        if (isset($_SESSION[$key])) {
+            return  $_SESSION[$key];
+        }
+        return null;
+    }
+
+    private static function has($key)
+    {
+        self::start();
+        return (isset($_SESSION[$key]));
+    }
+    private static function remove($key)
+    {
+        self::start();
+        if (isset($_SESSION[$key])) {
+            unset($_SESSION[$key]);
+        }
+    }
+
+    private static function remove_all(){
+      self::start();
+      session_unset();
+      session_destroy();
+    }
+
+    private static function get_all() {
+        self::start();
+        return $_SESSION ;
+    }
+}
 
 
 
@@ -15,3 +60,22 @@
 
 
 ?>
+
+
+<form action="" method="POST">
+    <input type="text" name="name" placeholder="type your name">
+
+
+    <input type="submit" value="send"><br>
+    <input type="submit" value="logout">
+
+
+
+
+
+
+
+
+
+
+</form>
